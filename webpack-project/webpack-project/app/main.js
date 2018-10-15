832145b9f30c4c075ec263492d140309cb98eda9
hjai <hjaiim@163.com>
2018-10-15 16:48:26 +0800

    js-轮播-未完成

--- webpack-project/webpack-project/app/main.js
+++
@@ -1,9 +0,0 @@
-﻿//const greeter = require('./Greeter.js');
-//document.querySelector("#root").appendChild(greeter());
-
-
-import React from 'react';
-import {render} from 'react-dom';
-import Greeter from './Greeter';
-
-render(<Greeter />, document.getElementById('root'));
